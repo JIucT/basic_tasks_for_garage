14833bc800df5b9e301240074efe11196bf6d96a
JIuct <dgoul11@gmail.com>
2014-06-11 22:28:22 +0300

    some fixes of Factory task

--- struct.rb
+++ struct.rb
@@ -1,15 +1,20 @@
 class Factory
-  def self.new(*params)
-    Class.new do 
+  def self.new(*params, &block)
+    Class.new do
       attr_accessor *params
-      def self.new(*values)        
-        instance = super()
-        keys = @fields_names
+
+      if block_given?
+        class_eval(&block)
+      end
+
+      define_singleton_method :new do |*values|
+        instance = super()        
+        keys = params
         instance.instance_eval do
           @fields = Hash[keys.zip values]
           @fields.each do |key, value|
             self.class.__send__(:define_method, key) do
-              @fields[key] 
+              @fields[key]
             end
           end
         end
@@ -33,51 +38,70 @@ class Factory
           @fields[index.to_sym] = value
         end
       end
-
-      if block_given?
-        yield
-      end
-      @fields_names = params
     end
   end
 end
 
 
-
-Customer = Struct.new(:name, :address, :zip)
+Customer = Factory.new(:name, :address, :zip)
 joe = Customer.new("Joe Smith", "123 Maple, Anytown NC", 12345)
+
+p joe.name
+p joe.zip 
 
 joe["name"] = "Luke"
-joe[:zip]   = "90210"
+joe[:zip] = "90210"
 
-p joe.name   #=> "Luke"
-p joe.zip    #=> "90210"
+p joe.name #=> "Luke"
+p joe.zip #=> "90210"
 
 
-Customer = Struct.new(:name, :address, :zip)
-joe = Customer.new("Joe Smith", "123 Maple, Anytown NC", 12345)
-joe.each {|x| puts(x) }
+Customer2 = Factory.new(:name, :address, :zip2)
+joe2 = Customer2.new("Joe Smith", "123 Maple, Anytown NC", 12345)
+ p joe.class
+p joe2.name 
+p joe2.zip2 
 
 # Joe Smith
 # 123 Maple, Anytown NC
 # 12345
 
+p joe.name #=> "Luke"
+p joe.zip #=> "90210"
 
-Customer = Struct.new(:name, :address, :zip)
+Customer = Factory.new(:name, :address, :zip)
  
 joe = Customer.new("Joe Smith", "123 Maple, Anytown NC", 12345)
- 
+
 p joe.name
 p joe["name"]
 p joe[:name]
-p joe[0]
+p joe[1]
 #"Joe Smith"
 
 
-Customer = Struct.new(:name, :address) do
+Customer = Factory.new(:name, :address) do
   def greeting
     "Hello #{name}!"
   end
 end
+
 p Customer.new("Dave", "123 Main").greeting
 # "Hello Dave!"
+
+s = Struct.new(:name)
+a = Factory.new(:name)
+
+class D < Factory.new(:haha)
+end
+class AA < Factory ; end
+t = AA.new(:name)
+p t.new("tt").name
+x = a.new("dfdf")
+p x['name']
+p D.new("DD").haha
+xx = a.new("dfdfxx")
+p x, xx
+#p s.new("dfdf")
+
+#p a.new.ancestors
